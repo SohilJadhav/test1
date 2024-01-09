6e84b9c99468817d837f9be2a9cc18577a31537b
Sohil Jadhav <sohil.jadhav.04@gmail.com>
2024-01-09 12:57:55 +0530

    session 2 first commit

---
+++ lp.py
@@ -0,0 +1,9 @@
+# Example 1: Iterating over a range of numbers
+for i in range(5):
+    print(i)
+    print(i)
+# Example 2: Iterating over elements in a list
+fruits = ["apple", "banana", "orange","mango"]
+for fruit in fruits:
+    print(fruit)
+    print(i)
