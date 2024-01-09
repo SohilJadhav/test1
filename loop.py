94b0b93a093a2774ec180e078cbde2b8f530c2b0
Sohil Jadhav <sohil.jadhav.04@gmail.com>
2024-01-09 12:33:46 +0530

    session2 first commit

--- loop.py
+++ loop.py
@@ -1,7 +1,7 @@
 # Example 1: Iterating over a range of numbers
 for i in range(5):
     print(i)
-
+    print(i)
 # Example 2: Iterating over elements in a list
 fruits = ["apple", "banana", "orange"]
 for fruit in fruits:
